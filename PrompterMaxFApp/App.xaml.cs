59d427bfa430efec68dad4974d8dce448788e65b
edwar <edwardjcw@gmail.com>
2022-03-09 06:47:19 -0600

    add changes

---
+++ PrompterMaxFApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrompterMaxFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Activated += App_Activated;
+        }
+
+        private void App_Activated(object sender, EventArgs e)
+        {
+            Activated -= App_Activated;
+            Prompter.AppMain.main(MainWindow, () => new ExtractorWindow(), () => new PrompterWindow());
+        }
+    }
+}
